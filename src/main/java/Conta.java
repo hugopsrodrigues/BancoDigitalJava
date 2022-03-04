507b44453bd4cf0fa03f352e464ea35797e81111
Hugo Rodrigues <hugopsr@hotmail.com>
2022-03-04 19:01:17 -0300

    Initial commit

---
+++ src/main/java/Conta.java
@@ -0,0 +1,20 @@
+public class Conta {
+
+    private int agencia;
+    private int conta;
+    private double saldo;
+    private String tipo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+
+}
