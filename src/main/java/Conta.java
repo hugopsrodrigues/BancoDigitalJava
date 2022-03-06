fce6297297798b2301bac9f53e165ff192d1fad0
Hugo Rodrigues <hugopsr@hotmail.com>
2022-03-06 14:18:30 -0300

    commit inicial

--- src/main/java/Conta.java
+++ src/main/java/Conta.java
@@ -1,20 +1,52 @@
-public class Conta {
+public abstract class Conta implements IConta{
 
-    private int agencia;
-    private int conta;
-    private double saldo;
-    private String tipo;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int conta;
+    protected double saldo;
+    //private String tipo;
 
-    public void sacar(){
-
+    public Conta(){
+        this.agencia = agencia;
+        this.conta = SEQUENCIAL++;
     }
 
-    public void depositar(){
-
+    public int getAgencia() {
+        return agencia;
     }
 
-    public void transferir(){
-
+    public int getConta() {
+        return conta;
     }
 
+    public double getSaldo() {
+        return saldo;
+    }
+
+//    public String getTipo() {
+//        return tipo;
+//    }
+
+    @Override
+    public void sacar(double valor) {
+        this.saldo -= valor;
+    }
+
+    @Override
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+
+    @Override
+    public void transferir(double valor, Conta contaDestino) {
+        this.sacar(valor);
+        contaDestino.depositar(valor);
+    }
+
+    protected void imprimirExtratoConta() {
+        System.out.printf("Agencia: %d\n", this.agencia);
+        System.out.printf("Conta: %d\n", this.conta);
+        System.out.printf("Saldo: %.2f\n", this.saldo);
+    }
 }
